699df21fd12d74a3f70de598007c2e6bb54e9d9d
ScottTpirate <44659733+ScottTpirate@users.noreply.github.com>
2024-11-28 12:08:05 -0600

    Release version 0.2.0-beta.1: Added visualization module and refactored code.

--- refractulator/__init__.py
+++ refractulator/__init__.py
@@ -3,9 +3,7 @@
 from .calculate import Refractulator
 from .data_generator import RefractulatorDataGenerator
 from .visualization import visualize, visualize_2d, visualize_3d
-from ._version import version as __version__
-
-
+# from ._version import version as __version__
 
 
 __version__ = "0.2.0-beta.1"  # Ensure this matches pyproject.toml
